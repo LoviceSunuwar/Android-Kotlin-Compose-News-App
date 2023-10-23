6a8f66608b9143419f1cfe4fc87941114c6cdc70
LoviceSunuwar <Lovicesunuwar@gmail.com>
2023-10-23 15:11:55 -0400

    Pushing the base project of jetpack compose

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Kotlin Compose New App"
+include(":app")
+ 
